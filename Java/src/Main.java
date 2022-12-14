1f3f8d0a8eb91ff4b56ecdde2c8e662c6a4f1f76
Platon <platon.solovev05@gmail.com>
2022-12-14 21:31:35 +0300

    0.0.1

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,5 +1,20 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.ArrayList;
+
+public class Main{
+    public static void main(String[] args){
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i <= 10000000;i++){
+            if (is_prime(i)){
+                arrayList.add(i);
+            }
+        }
+        System.out.print(arrayList);
+    }
+    public static boolean is_prime(int num){
+        int d = 2;
+        while (d * d <= num && num % d != 0){
+            d++;
+        }
+        return d * d > num;
     }
 }
